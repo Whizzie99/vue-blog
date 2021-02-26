4f1616c16454deda205bb48c418d1f91dbf627c2
Whizzie99 <47413415+Whizzie99@users.noreply.github.com>
2021-02-26 16:41:56 +0100

    integrated json server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="home">
-
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts"/>
+    </div>
+    <div v-else>Loading...</div>
+    <!-- <button @click="showPosts = !showPosts">toggle post</button> -->
   </div>
 </template>
 
 <script>
-
+import PostList from '../components/PostList.vue'
+import getPosts from '../composables/getPosts'
 
 export default {
+
   name: 'Home',
   components: {
+    PostList
+  },
+  setup(){
     
-  }
+    const {posts, error, load} = getPosts()
+
+    load()
+
+    return{ posts, error }
+  },
+  
 }
 </script>
